28e56f309c8c2cb839767fa692ecd48c265ae23e
섭 <118520116+seob12@users.noreply.github.com>
2024-10-18 04:08:00 +0900

    [Silver III] Title: 가희와 키워드, Time: 1256 ms, Memory: 150488 KB -BaekjoonHub

---
+++ 백준/Silver/22233. 가희와 키워드/가희와 키워드.py
@@ -0,0 +1,19 @@
+import sys
+
+key_n, post_n = map(int, sys.stdin.readline().split(' '))
+
+memo = {}
+answer = 0
+
+for _ in range(key_n):
+    memo[sys.stdin.readline().rstrip()] = 1
+    answer += 1
+
+for _ in range(post_n):
+    post = list(sys.stdin.readline().rstrip().split(','))
+    for i in post:
+        if i in memo.keys():
+            if memo[i] == 1:
+                memo[i] = 0
+                answer -= 1
+    print(answer)
