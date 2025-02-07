97a7d1b563651271f8351ed9199179c17534e3d4
섭 <118520116+seob12@users.noreply.github.com>
2025-02-07 21:45:09 +0900

    [Silver III] Title: 진우의 달 여행 (Small), Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/17484. 진우의 달 여행 （Small）/진우의 달 여행 （Small）.py
+++ 백준/Silver/17484. 진우의 달 여행 （Small）/진우의 달 여행 （Small）.py
@@ -1,27 +1,29 @@
 n,m = map(int,input().split())
-arr = []
-for _ in range(n):
-    arr.append(list(map(int,input().split())))
 
-#arr.insert(0, [0]*m)
-#arr.append([0]*m)
+arr = [list(map(int,input().split())) for _ in range(n)]
 
-def dfs(col, row, d, low, answer):  # 백트래킹
+def dfs(i,j,direction,mid,total):
     dir = [-1,0,1]
 
-    if col == n-1:
-        return min(low, answer)
+    if i == n-1:
+        tmp = min(mid,total)
+        return tmp
 
-    for i in dir:
-        if i != d:
-            if 0 <= col < n and 0 <= row + i < m:
-                low = dfs(col+1, row+i, i, low, answer+arr[col+1][row+i])
-    return low
+    for d in dir:
+        if d == direction:
+            continue
+        else:
+            nx = i + 1
+            ny = j + d
+
+            if 0<=nx<n and 0<=ny<m:
+                mid = dfs(nx,ny,d,mid,total + arr[nx][ny])
+
+    return mid
 
 
+ans = 1e9
+for j in range(m):
+    ans = min(ans,dfs(0,j,-2,ans,arr[0][j]))
 
-low = 1e9
-for i in range(m):
-    low = min(dfs(0,i,2,low,arr[0][i]), low)
-
-print(low)
+print(ans)
