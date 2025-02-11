985a5cd133f8ff5099d1352e47e0abfb2d36c308
섭 <118520116+seob12@users.noreply.github.com>
2025-02-12 01:43:49 +0900

    [Silver I] Title: 미로 탐색, Time: 120 ms, Memory: 114144 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,41 +1,39 @@
 from collections import deque
 
-n,m = map(int,input().split())
-
-arr = [list(map(int,input())) for _ in range(n)]
-v = [[0]*m for _ in range(n)]
-cnt = 1e9
-
-di = [0,0,1,-1]
-dj = [1,-1,0,0]
-
 def in_range(x,y):
     return 0<=x<n and 0<=y<m
 
-def bfs(x,y):
+def bfs(i,j):
+    #global ans
     q = deque()
-    v[x][y] = 1
-    q.append((x,y))
+    q.append((i,j))
+    #v = [[0]*m for _ in range(n)]
+    #v[i][j] = 1
+
+    di = [0,0,1,-1]
+    dj = [1,-1,0,0]
+    #cnt = 0
 
     while q:
-        i,j = q.popleft()
+        x,y = q.popleft()
+
+        if x == n-1 and y == m-1:
+            return arr[x][y]
 
         for d in range(4):
-            nx = i + di[d]
-            ny = j + dj[d]
+            nx = x + di[d]
+            ny = y + dj[d]
 
             if not in_range(nx,ny):
                 continue
             else:
-                if v[nx][ny] == 0 and arr[nx][ny] == 1:
-                    arr[nx][ny] = arr[i][j] + 1
-                    v[nx][ny] = 1
+                if arr[nx][ny] == 1:
                     q.append((nx,ny))
-
-    return arr[n-1][m-1]
-
-
-re = bfs(0,0)
+                    arr[nx][ny] = arr[x][y] + 1
 
 
-print(re)
+n,m = map(int,input().split())
+
+arr = [list(map(int,input())) for _ in range(n)]
+
+print(bfs(0,0))
